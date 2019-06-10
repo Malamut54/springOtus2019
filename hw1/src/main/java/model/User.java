5a69d52ef5e90a4d71d612d7b5447cd0e8ac3d4d
Bobrov Dmitriy <bobrov.dmitriy@gmail.com>
2019-06-10 11:43:49 +0300

    hw_01 модель

---
+++ hw1/src/main/java/model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+public class User {
+    private String firstName;
+    private String secondName;
+
+    public User(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + secondName;
+    }
+}

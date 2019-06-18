c0ea66fa4730f9c9299792dba0dee214b9e7f9fa
Bobrov Dmitriy <bobrov.dmitriy@gmail.com>
2019-06-18 18:15:41 +0300

    hw_03 Main class

--- hw3/src/main/java/ru/otus/boot/hw3/Hw3Application.java
+++ hw3/src/main/java/ru/otus/boot/hw3/Hw3Application.java
@@ -2,12 +2,15 @@ package ru.otus.boot.hw3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.otus.boot.hw3.service.InputOutputServiceImpl;
 
 @SpringBootApplication
 public class Hw3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hw3Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Hw3Application.class, args);
+        context.getBean("inputOutputServiceImpl", InputOutputServiceImpl.class).startExam();
     }
 
 }

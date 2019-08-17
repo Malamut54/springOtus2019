ddfa0d3aa94466cc16742300130fc69d5744d82d
Bobrov Dmitriy <bobrov.dmitriy@gmail.com>
2019-08-17 18:30:05 +0300

    hw_07 Тесты

---
+++ hw07/src/test/java/ru/otus/boot/hw7/service/author/AuthorServiceImplTest.java
@@ -0,0 +1,130 @@
+package ru.otus.boot.hw7.service.author;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import ru.otus.boot.hw7.exception.EmptyListException;
+import ru.otus.boot.hw7.exception.ExistEntityException;
+import ru.otus.boot.hw7.model.Author;
+
+import javax.transaction.Transactional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@SpringBootTest(properties = {
+        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
+@Transactional
+class AuthorServiceImplTest {
+    @Autowired
+    private AuthorService authorService;
+
+    @BeforeEach
+    public void prepare() {
+        authorService.insert(new Author("Stephen", "King"));
+        authorService.insert(new Author("Stephen", "Balmer"));
+        authorService.insert(new Author("Fedor", "Dostoevskiy"));
+        authorService.insert(new Author("Viktor", "Pelevin"));
+
+    }
+
+    @AfterEach
+    public void del() {
+        authorService.delete(new Author("Stephen", "King"));
+        authorService.delete(new Author("Stephen", "Balmer"));
+        authorService.delete(new Author("Fedor", "Dostoevskiy"));
+        authorService.delete(new Author("Viktor", "Pelevin"));
+    }
+
+    @Test
+    @DisplayName("Get All authors")
+    public void testGetAllAuthors() {
+        assertThat(authorService.getAll().stream().map(Author::getLastName).collect(Collectors.toList()))
+                .containsOnly("King", "Balmer", "Dostoevskiy", "Pelevin");
+    }
+
+    @Test
+    @DisplayName("Get authors by first name")
+    public void testGetAuthorByFirstName() {
+        assertThat(authorService.getByFirstName("Stephen").stream()
+                .map(Author::getLastName)
+                .collect(Collectors.toList()))
+                .containsOnly("King", "Balmer");
+    }
+
+    @Test
+    @DisplayName("Get authors by wrong first name")
+    public void testGetAuthorByWrongFirstName() {
+        assertThatExceptionOfType(EmptyListException.class).isThrownBy(() -> authorService.getByFirstName("asd"))
+                .withMessage("Nothing to found");
+    }
+
+    @Test
+    @DisplayName("Get authors by last name")
+    public void testGetAuthorByLastName() {
+        assertThat(authorService.getByLastName("King").stream()
+                .map(Author::getFirstName)
+                .collect(Collectors.toList()))
+                .containsOnly("Stephen");
+    }
+
+    @Test
+    @DisplayName("Get authors by wrong last name")
+    public void testGetAuthorByWrongLastName() {
+        assertThatExceptionOfType(EmptyListException.class).isThrownBy(() -> authorService.getByLastName("asd"))
+                .withMessage("Nothing to found");
+    }
+
+    @Test
+    @DisplayName("When Author is exist")
+    public void testIsAuthorIsExist() {
+        assertThat(authorService.isExist("Stephen", "King")).isTrue();
+    }
+
+    @Test
+    @DisplayName("When Author is not exist")
+    public void testIsAuthorIsNotExist() {
+        assertThat(authorService.isExist("Test", "testovich")).isFalse();
+    }
+
+    @Test
+    @DisplayName("Insert new Author")
+    public void testInsertNewAuthor() {
+        authorService.insert(new Author("Author", "Author"));
+        assertThat(authorService.isExist("Author", "Author")).isTrue();
+
+        authorService.delete(new Author("Author", "Author"));
+    }
+
+    @Test
+    @DisplayName("Insert Author duplicate")
+    public void tsetInsertAuthorWithDuplicate() {
+        assertThatExceptionOfType(ExistEntityException.class).isThrownBy(() ->
+                authorService.insert(new Author("Stephen", "King"))).withMessage("This author is exist");
+    }
+
+    @Test
+    @DisplayName("Delete author when author is exist")
+    public void testDeleteAuthorWhenExist() {
+        Author author = authorService.getByFirstNameAndLastName("Stephen", "King");
+        authorService.delete(author);
+        assertThat(authorService.isExist("Stephen", "King")).isFalse();
+        authorService.insert(new Author("Stephen", "King"));
+    }
+
+    @Test
+    @DisplayName("Delete author when author is not exist")
+    void testDeleteAuthorWhenNotExist() {
+        assertThatExceptionOfType(ExistEntityException.class).isThrownBy(() ->
+                authorService.delete(new Author("asdd", "adwq")))
+                .withMessage("asdd adwq is not exist!");
+    }
+}
---
+++ hw07/src/test/java/ru/otus/boot/hw7/service/genre/GenreServiceImplTest.java
@@ -0,0 +1,101 @@
+package ru.otus.boot.hw7.service.genre;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import ru.otus.boot.hw7.exception.ExistEntityException;
+import ru.otus.boot.hw7.model.Genre;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@SpringBootTest(properties = {
+        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
+@Transactional
+class GenreServiceImplTest {
+    @Autowired
+    private GenreService genreService;
+
+    @BeforeEach
+    public void prepare() {
+        genreService.insert(new Genre("classic"));
+        genreService.insert(new Genre("horrors"));
+        genreService.insert(new Genre("roman"));
+    }
+
+    @AfterEach
+    public void del() {
+        genreService.delete(new Genre("classic"));
+        genreService.delete(new Genre("horrors"));
+        genreService.delete(new Genre("roman"));
+    }
+
+    @Test
+    @DisplayName("Get all genres")
+    public void testGetAllGenres() {
+        List<String> collect = genreService.getAll().stream().map(Genre::getGenreName).collect(Collectors.toList());
+        assertThat(collect).contains("classic", "horrors", "roman");
+    }
+
+    @Test
+    @DisplayName("Get genre by title")
+    public void testGetGenreByTitle() {
+        assertThat(genreService.getByTitle("roman").getGenreName()).isEqualTo("roman");
+    }
+
+    @Test
+    @DisplayName("Get genre by wrong title")
+    public void testGetGenreByWrongTitle() {
+        assertThatExceptionOfType(ExistEntityException.class).isThrownBy(() ->
+                genreService.getByTitle("asd")).withMessage("asd is not exist!");
+    }
+
+    @Test
+    @DisplayName("When Genre is exist")
+    public void testGenreIsExit() {
+        assertThat(genreService.isExist("roman")).isTrue();
+    }
+
+    @Test
+    @DisplayName("When Genre is not exist")
+    public void testGenreIsNotExit() {
+        assertThat(genreService.isExist("asdf")).isFalse();
+    }
+
+    @Test
+    @DisplayName("Insert genre")
+    public void testInsertGenre() {
+        genreService.insert(new Genre("computer science"));
+        assertThat(genreService.isExist("computer science")).isTrue();
+        genreService.delete(new Genre("computer science"));
+    }
+
+    @Test
+    @DisplayName("Delete genre when genre is exist")
+    public void testDeleteGenreWhenExist() {
+        Genre genre = new Genre("genre");
+        genreService.insert(genre);
+        assertThat(genreService.isExist("genre"));
+
+        genreService.delete(genre);
+        assertThat(genreService.isExist("genre")).isFalse();
+    }
+
+    @Test
+    @DisplayName("Delete genre when genre is not exist")
+    public void testDeleteGenreWhenNotExist() {
+        assertThatExceptionOfType(ExistEntityException.class).isThrownBy(() ->
+                genreService.delete(new Genre("Asa"))).withMessage("Asa is not exist!");
+    }
+}
